5d60266a3950341e70b39bdb957380188f9aa735
Jesper Kamstrup Linnet <jesper@kamstrup-linnet.dk>
2009-10-19 19:59:40 +0200

    Updated presentation

--- MapScript.scala
+++ MapScript.scala
@@ -4,9 +4,10 @@ println(persons)
 val personsWithB = persons filterKeys { _ startsWith "B" }
 println("Persons with B: " + personsWithB)
 
-val personsOver40 = persons filter { element =>
-        val (name, age) = element
-        age > 40 }
+val personsOver40 = persons filter { _._2 > 40 }
+//val personsOver40 = persons filter { element =>
+//        val (name, age) = element
+//        age > 40 }
 println("Persons over 40: " + personsOver40)
 
 val benny = persons.get("Benny")
